3442475a1cec22e18f6f25eafb9ef6b1a89a2dae
sten <stanislaw89@gmail.com>
2019-03-04 15:10:50 +0200

    fix chat not sticking to end

--- Assets/Demo/Scripts/ChatDemo.cs
+++ Assets/Demo/Scripts/ChatDemo.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using SG;
 using UnityEngine;
 using UnityEngine.UI;
@@ -84,8 +82,8 @@ public class ChatDemo : MonoBehaviour
 
     public void SendChatMessage()
     {
-        messages.Add(inputField.text);
         var positionAtEnd = scroll.normalizedPosition.y >= 1;
+        messages.Add(inputField.text);
         UpdateScroll();
         if (positionAtEnd)
         {
